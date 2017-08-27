06a35ea071f24d475ab10e2cb4cca7a1c691af8c
dodders <george.dodwell@gmail.com>
2017-08-27 19:30:44 -0400

    initial chartjs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,50 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Line} from 'react-chartjs-2';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+var cdata = {
+    labels: ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"],
+    datasets: [{
+        label: 'Hi Temp',
+        data: [77, 79, 65, 67, 70, 85],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+    }]
+}
+
+var copts = {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+    }
+}
+
+class MyChart extends React.Component {
+    render() {
+        return <Line data={cdata} options={copts} width="200" height="200" />
+    }    
+}
+
+ReactDOM.render(<MyChart />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

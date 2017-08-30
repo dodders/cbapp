bf2f71ee6116715584766cc5e63adfdf20801d19
George Dodwell <george.dodwell@gmail.com>
2017-08-29 22:16:19 -0400

    remove node modules

--- src/index.js
+++ src/index.js
@@ -1,52 +1,92 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {Line} from 'react-chartjs-2';
-
 
 var cdata = {
     labels: ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"],
     datasets: [{
         label: 'Hi Temp',
+        fill: false,
         data: [77, 79, 65, 67, 70, 85],
-        backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-    }]
+        borderColor: '#ff0000',
+        borderWidth: 2
+    },
+    {
+        label: 'Low Temp',
+        fill: false,
+        data: [57, 60, 55, 56, 60, 65],
+        borderColor: '#000076',
+        borderWidth: 2
+        }]
 }
 
 var copts = {
     scales: {
         yAxes: [{
             ticks: {
-                beginAtZero:true
+                beginAtZero:false,
+                min: 30
             }
         }]
+    },
+    layout: {
+        padding: {
+            left: 50, right: 50, top: 10, bottom: 10
+        }
+    },
+    legend: {
+        display: false
+    }
+}
+
+class Page extends React.Component {
+    render() {
+        return (
+            <div>
+                <div><Header /></div>
+                <div><Temps /></div>
+                <div><MyChart /></div>
+            </div>
+        );
     }
 }
 
 class MyChart extends React.Component {
     render() {
-        return <Line data={cdata} options={copts} width="200" height="200" />
+        return <Line data={cdata} options={copts} width="100" height="50" />
     }    
 }
 
-ReactDOM.render(<MyChart />, document.getElementById('root'));
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="header">
+                <a href="#menu" className="box-shadow-menu" alt="" />
+                Maine
+            </div>
+        );
+    }
+}
+
+class Temps extends React.Component {
+    render() {
+        return (
+            <div className="top">
+                <div className ="left">
+                    <span className="big">65F</span><br/>
+                    <span className="small">Living Room</span>
+                </div>
+                <div className ="right">
+                    <span className="big">55F</span><br/>
+                    <span className="small">Cellar</span>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Page />, document.getElementById('root'));
 //ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
